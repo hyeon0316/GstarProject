26d374777d93209248713163e8a30686713c85b8
hyeon0316 <kkhh7027@naver.com>
2021-10-26 14:44:35 +0900

    아이템 사용창 구현중

--- Assets/InvenTory/Scripts/UIScripts/UseSlotBase.cs
+++ Assets/InvenTory/Scripts/UIScripts/UseSlotBase.cs
@@ -10,9 +10,46 @@ public class UseSlotBase : MonoBehaviour
     private GameObject go_UseSlotParent; //슬롯의 부모객체
 
     public Use_Slot[] uSlots;
+    private Use_Slot uSlot;
     // Start is called before the first frame update
     void Start()
     {
         uSlots = go_UseSlotParent.GetComponentsInChildren<Use_Slot>();
+        uSlot = FindObjectOfType<Use_Slot>();
+    }
+    void Update()
+    {
+        CommendNumber();
+    }
+
+    private void CommendNumber()
+    {
+        for (int i = 0; i < uSlots.Length; i++)
+        {
+            if (i == 0)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    uSlot.UseItem();
+                    Debug.Log(1);
+                }
+            }
+            else if (i == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    uSlot.UseItem();
+                    Debug.Log(2);
+                }
+            }
+            else if (i == 2)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    uSlot.UseItem();
+                    Debug.Log(3);
+                }
+            }
+        }
     }
 }
--- Assets/InvenTory/Scripts/UIScripts/Use_Slot.cs
+++ Assets/InvenTory/Scripts/UIScripts/Use_Slot.cs
@@ -30,36 +30,10 @@ public class Use_Slot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
     void Update()
     {
-        CommendNumber();
-    }
-    private void CommendNumber()
-    {
-        for (int i = 0; i < transform.parent.GetComponent<UseSlotBase>().uSlots.Length; i++) //슬롯 자리 검사
-        {
-            if (transform.parent.GetComponent<UseSlotBase>().uSlots[i].item != null)
-            {
-                switch(i)
-                {
-                    case 0:
-                        if (Input.GetKeyDown(KeyCode.Alpha1))
-                        {
-                            if (item.itemType == Item.ItemType.Used)
-                            {
-                                if (item.itemName == "Potion_Hp")
-                                {
-                                    if (thePlayer.startingHealth > thePlayer.health)
-                                    {
-                                        thePlayer.HealHp(100);
-                                        SetSlotCount(-1);
-                                    }
-                                }
-                            }
-                        }
-                        break;
-                }
-            }
-        }
-    }
+        
+    }   
+    
+
     private void SetColor(float _alpha) //이미지 투명도 조절
     {
         Color color = itemImage.color;
@@ -85,6 +59,28 @@ public class Use_Slot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
         if (itemCount <= 0)
             ClearSlot();
+    }
+    public void UseItem()
+    {
+        if (item.itemType == Item.ItemType.Used)
+        {
+            if (item.itemName == "Potion_Hp")
+            {
+                if (thePlayer.startingHealth > thePlayer.health)
+                {
+                    thePlayer.HealHp(100);
+                    SetSlotCount(-1);
+                }
+            }
+            else if (item.itemName == "Potion_Mp")
+            {
+                if (thePlayer.startingMana > thePlayer.mana)
+                {
+                    thePlayer.HealMp(100);
+                    SetSlotCount(-1);
+                }
+            }
+        }
     }
 
     public void ClearSlot()//슬롯 초기화
@@ -127,7 +123,7 @@ public class Use_Slot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         if (DragSlot_Used.instance.dragSlot_Used != null)//빈슬롯을 드래그해서 Null 참조를 발생하는 것을 방지    
             ChangeSlot();
 
-        if (DragSlot.instance.dragSlot != null)
+        if (DragSlot.instance.dragSlot != null && DragSlot.instance.dragSlot.item.itemType == Item.ItemType.Used)//소모품만 드롭가능
         {
             Inter_ChangeSlot();
         }
@@ -149,11 +145,12 @@ public class Use_Slot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     private void Inter_ChangeSlot() //인벤창에서 아이템사용창으로 드래그
     {
         Item _tempItem = item;
+        int _tempItemCount = itemCount;
 
-        AddItem(DragSlot.instance.dragSlot.item);
+        AddItem(DragSlot.instance.dragSlot.item, DragSlot.instance.dragSlot.itemCount);
 
         if (_tempItem != null)
-            DragSlot.instance.dragSlot.AddItem(_tempItem);
+            DragSlot.instance.dragSlot.AddItem(_tempItem, _tempItemCount);
         else
             DragSlot.instance.dragSlot.ClearSlot();
     }
