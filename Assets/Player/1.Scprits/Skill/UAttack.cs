55024979b2df3804506c1ce02aaa50199fd31bcd
kiom485 <ehdrua485@naver.com>
2021-10-26 21:51:40 +0900

    ㅅㄷㄴㅅ1

--- Assets/Player/1.Scprits/Skill/UAttack.cs
+++ Assets/Player/1.Scprits/Skill/UAttack.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class UAttack : AttackState
 {
+
     float dmg;
     float startingDmg = 0f;
     void Awake()
     {
         dmg = Player.inst.power + startingDmg;
     }
-
+    
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("dd");
         TagCheck(other, dmg);
     }
+    
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Debug.Log("[Enemy] , 데미지 :" + dmg);
+            OnDamageEvent(other.gameObject);
+        }
+        if (other.tag == "Boss")
+        {
+            Debug.Log("[BOss] , 데미지 :" + dmg);
+            OnDamageEvent(other.gameObject);
+        }
+    }
+
+    public void OnDamageEvent1(GameObject targetEntity)
+    {
+        //공격 대상을 지정할 추적 대상의 LivingEntity 컴포넌트 가져오기
+        LivingEntity attackTarget = targetEntity.GetComponent<LivingEntity>();
+
+        //공격 처리(플레이어에게)
+        attackTarget.OnDamage(dmg);
+        Destroy(gameObject);
+    }
+    */
 }
