2942b9892bb58ac8ecf0d2de40c4e9fab4d5bf38
hyeon0316 <kkhh7027@naver.com>
2021-11-08 21:39:09 +0900

    보스 아이템 추가 및 타이틀 제작중

---
+++ Assets/Intro&Outro/TitleBtn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleBtn : MonoBehaviour
+{
+    [SerializeField]
+    private Image startFade;
+
+    public void GameStart()
+    {
+        StartCoroutine(StartFade());
+        SceneManager.LoadScene("Intro");
+    }
+   
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator StartFade()
+    {
+        yield return null;
+        Color color = startFade.color;
+        color.a = 0;
+    }
+
+}
